b71abb33937f4404decbc6e66b531c67643ed8fc
EzequielVillalba112 <148280303+EzequielVillalba112@users.noreply.github.com>
2023-11-13 17:55:16 -0300

    Validación de contacto

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,90 @@
+const nameContact = document.getElementById("name");
+const emailContact = document.getElementById("email");
+const messageContact = document.getElementById("message");
+const buttonSendMessage = document.querySelector(".btn-send-email");
+const alertContact = document.querySelector(".alert");
+
+
+const validEmptyField = () =>{
+    if(nameContact.value == "" || emailContact.value == "" || messageContact.value == ""){
+
+        if(nameContact.value == ""){
+            nameContact.classList.add("error");
+        }
+        if(emailContact.value == ""){
+            emailContact.classList.add("error");
+        }
+        if(messageContact.value == ""){
+            messageContact.classList.add("error");
+        }
+
+        alertContact.innerHTML = `Empty fields!!!`;
+
+        setTimeout(()=>{
+           nameContact.classList.remove("error");
+           emailContact.classList.remove("error");
+           messageContact.classList.remove("error");
+           alertContact.innerHTML = " ";
+        },2000);
+    
+        return false;
+    }else if(validFielContent() == 1){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+
+const validFielContent = () =>{
+    const nameValid = /^[a-zA-Z\s]+$/;
+    const emailValid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
+    let bande = 0;
+
+    if(!nameValid.test(nameContact.value)){
+        const spanName = document.querySelector(".span-name");
+        nameContact.classList.add("error");
+        spanName.innerHTML = `Invalid name!!!`;
+       setTimeout(()=>{
+            spanName.innerHTML = "";
+            nameContact.classList.remove("error");
+       },3000);
+       bande = 1;
+    }
+
+    if(!emailValid.test(emailContact.value)){
+        const spanEmail = document.querySelector(".span-email");
+
+        emailContact.classList.add("error");
+        spanEmail.innerHTML = `Invalid email!!!`;
+
+       setTimeout(()=>{
+            spanEmail.innerHTML = "";
+            emailContact.classList.remove("error");
+       },3000);
+       bande = 1;
+    }
+
+    return bande;
+}
+
+const clearInput = () => {
+    nameContact.value = "";
+    emailContact.value = "";
+    messageContact.value = "";
+}
+
+buttonSendMessage.addEventListener("click", ()=>{
+    if(validEmptyField){
+        sendMessage();
+        clearInput()
+        alert("send message");
+    }
+})
+
+
+
+
 function sendMessage(){
     (function(){
         emailjs.init("Os-XuWVdt39Zoik9k");
@@ -7,9 +94,9 @@ function sendMessage(){
     var templateId = "template_q45kdsg";
 
     var params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value
+        name: nameContact.value,
+        email: emailContact.value,
+        message: messageContact.value
     }
 
     emailjs.send(serviceId, templateId, params).then(function(response) {
